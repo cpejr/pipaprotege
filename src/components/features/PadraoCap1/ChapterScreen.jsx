cd4c3dbb0ff07f619aa06bfa1dac8d8e2a98dacc
matheusmadeira <fumacammadeira@gmail.com>
2025-11-04 09:05:36 -0300

    fix: button

--- src/components/features/PadraoCap1/ChapterScreen.jsx
+++ src/components/features/PadraoCap1/ChapterScreen.jsx
@@ -31,7 +31,7 @@ const ChapterScreen = ({
       <BotaoContainer>
         {showOtherButton && onOtherButtonClick && (
           <Button type='voltar' onClick={onOtherButtonClick}>
-            Agora não
+            AGORA NÃO
           </Button>
         )}
 
