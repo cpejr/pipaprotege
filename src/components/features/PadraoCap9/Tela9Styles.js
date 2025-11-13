9de70947ec3a404d5716c5baa48b0de9b9328961
Matheus Magalhães Madeira <matheusmadeira@148-239-238-200.rsf.ufmg.br>
2025-11-13 08:42:02 -0300

    .

--- src/components/features/PadraoCap9/Tela9Styles.js
+++ src/components/features/PadraoCap9/Tela9Styles.js
@@ -42,7 +42,25 @@ const BalaoBase = styled.div`
   background-size: contain;
   background-repeat: no-repeat;
 `;
+export const StyledTituloFlutuante = styled.div`
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
+  background-color: white;
+  color: black;
+
+  padding: 15px 40px;
+  border-radius: 30px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  white-space: nowrap;
+  z-index: 50;
+`;
 export const BalaoImagemEsq = styled(BalaoBase)`
   top: 25%;
   left: 10%;
