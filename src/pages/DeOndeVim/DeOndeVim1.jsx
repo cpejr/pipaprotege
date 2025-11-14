fb6ce990e4306cb37f369208bae3bee079dcadea
matheusmadeira <fumacammadeira@gmail.com>
2025-11-14 10:12:42 -0300

    feat: pages functional

--- src/pages/DeOndeVim/DeOndeVim1.jsx
+++ src/pages/DeOndeVim/DeOndeVim1.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { CloseOutlined, ArrowRightOutlined } from "@ant-design/icons";
 import { Container, CloseButton, ArrowButton } from "./Styles";
-
+import { useNavigate } from "react-router-dom";
 export default function DeOndeVim1() {
+  const navigate = useNavigate();
   return (
     <Container bg='1'>
       <CloseButton to='/'>
         <CloseOutlined />
       </CloseButton>
-      <ArrowButton to='/de-onde-vim2' right>
+      <ArrowButton onClick={navigate("/de-onde-vim/2")} right>
         <ArrowRightOutlined />
       </ArrowButton>
     </Container>
