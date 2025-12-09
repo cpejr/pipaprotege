aee16148ee2b6cd0707b18d308fc6f7c6aee0ba0
LuisOtavi0 <luis-o-ss@hotmail.com>
2025-12-09 19:33:57 -0300

    fix: aproximando baloes

--- src/components/features/PadraoCap9/Tela9Styles.js
+++ src/components/features/PadraoCap9/Tela9Styles.js
@@ -37,8 +37,8 @@ export const CachorroOverlay = styled.img`
 
 const BalaoBase = styled.div`
   position: absolute;
-  width: 450px;
-  height: 300px;
+  width: 35vw;
+  height: 20vw;
   background-size: contain;
   background-repeat: no-repeat;
 `;
@@ -63,27 +63,27 @@ export const StyledTituloFlutuante = styled.div`
 `;
 export const BalaoImagemEsq = styled(BalaoBase)`
   top: 25%;
-  left: 15%;
+  left: 10%;
   background-image: url(${BalaoEsquerdo});
 `;
 
 export const BalaoImagemDir = styled(BalaoBase)`
   top: 25%;
-  right: 18%;
+  right: 12%;
   background-image: url(${Balao_De_Fala});
 `;
 
 export const TextoBalao = styled.p`
   position: absolute;
-  top: 41%;
-  left: 50%;
+  top: 46%;
+  left: 48%;
   transform: translate(-50%, -50%);
 
   width: 80%;
   margin: 0;
 
   text-align: center;
-  font-size: 20px;
+  font-size: 1.3vw;
   font-weight: bold;
   font-family: Arial;
   line-height: 1.4;
